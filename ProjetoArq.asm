e8527f4fdc11d7aaf79955e6f71bc696e7d548a4
Caio Torre <caiosanches@live.com>
2018-08-20 14:54:09 -0300

    Added basic search function
    Searches the stack for a matching timestamp

--- ProjetoArq.asm
+++ ProjetoArq.asm
@@ -1,10 +1,14 @@
 .data
+ReadString:	.space 	16	#Nome do Posto
+StructInfo:	.space	28	#Número dado em bytes: | Data-2 | QtdCombust�vel-2 | Pre�o-4 | Distancia-4 | NomePosto-16
 
-Cadastrar:      .asciiz "1. para cadastrar abastecimento;\n"
-Excluir:        .asciiz "2. para excluir abastecimento;\n"
-EAbastecimento: .asciiz "3. para exibir abastecimento;\n"
-EConsumoMedio:  .asciiz "4. para consumo médio;\n"
-EPrecoMedio:    .asciiz "5. para preço médio;\n\n"
+
+#Declaração de strings
+Cadastrar:      .asciiz "\n1.Cadastrar abastecimento;\n" 
+Excluir:        .asciiz "2.Excluir abastecimento;\n"
+EAbastecimento: .asciiz "3.Exibir abastecimento;\n"
+EConsumoMedio:  .asciiz "4.Consumo médio;\n"
+EPrecoMedio:    .asciiz "5.Preço médio;\n\n"
 DigiteOpcao:    .asciiz "Digite a opção desejada: "
 
 Ins_Dia:	.asciiz "Insira o dia do abastecimento: "
@@ -15,7 +19,12 @@ Ins_Qlmt:	.asciiz "Insira a quilometragem do carro: "
 Ins_Qntd:	.asciiz "Insira a quantidade de combustivel: "
 Ins_Prec:	.asciiz "Insira o preco por litro: "
 
+SemReg:		.asciiz "Não há registro de abastecimento, retornando ao menu...\n"
+
+Ex_Qlmt:	.asciiz "Quilometragem: "
 .text
+#----------- Inicializando -----------#
+	and $s7,$s7,$zero	#"Seta" $s7 para 0 pois este contar� quantos cadastros foram feitos	
 #------------ Exibir Menu ------------#
 Menu:
 	li  $v0, 4
@@ -39,39 +48,110 @@ Menu:
 
 #------------ Exibir Menu ------------#	
 
-#--------- Opção Selecionada ---------#		
+#--------- Op��o Selecionada ---------#		
 	li $v0, 5
 	syscall
 	
-	addi $t1, $zero, 1
-	beq $t1, $v0, Cadastro
+	beq $v0,1,Cadastro
+	beq $v0,2,Exclui
+	beq $v0,3,EAbastece
+	beq $v0,4,EConsumo
+	beq $v0,5,EMedio
+		
+	j  Menu
 	
-	addi $t1, $zero, 2
-	beq $t1, $v0, Exclui
-	
-	addi $t1, $zero, 3
-	beq $t1, $v0, EAbastece
-	
-	addi $t1, $zero, 4
-	beq $t1, $v0, EConsumo
-	
-	addi $t1, $zero, 5
-	beq $t1, $v0, EMedio
-	
-	j Menu
-	
-#--------- Opção Selecionada ---------#	
+#--------- Op��o Selecionada ---------#	
 	
 #------ Cadastro Abastecimento -------#	
 Cadastro:
+	jal RData	
+	add $t1,$zero,$v0 # $t1 det�m do valor da EPOCH
+	
+	li $v0,4	#Recebe Nome do Posto no Addr. ReadString
+	la $a0,Ins_Nome
+	syscall
+	li $v0,8
+	la $a0,ReadString
+	li $a1,16
+	syscall
+	
+	li $v0,4	#Recebe Quilometragem em $s1
+	la $a0,Ins_Qlmt
+	syscall
+	li $v0,5
+	syscall
+	add $s1,$zero,$v0
+	
+	li $v0,4	#Recebe Quantidade de combust�vel em upper($s0)
+	la $a0,Ins_Qntd
+	syscall
+	li $v0,5
+	syscall
+	sll $v0,$v0,16
+	or $t1,$t1,$v0
+	
+	li $v0,4	#Recebe Pre�o do litro em $f0
+	la $a0,Ins_Prec
+	syscall
+	li $v0,6
+	syscall
+	
+	la $s4,ReadString
+	addi $sp,$sp,-28
+	
+	sw $t1,0($sp)	#Data e Qtd Comb. OK
+	s.s $f0,4($sp)	#Pre�o do litro	OK
+	sw $s1,8($sp)	#Km Atual	O
+	addi $sp,$sp,0xc
+	addi $t0,$zero,4
+StoreWord:		#Nome do Posto
+	lw $t1,0($s4)
+	sw $t1,0($sp)
+	addi $s4,$s4,4
+	addi $sp,$sp,4
+	addi $t0,$t0,-1
+	bnez $t0,StoreWord
+	
+	addi $sp,$sp,-28
+	addi $s7,$s7,1
+		
+	j Menu	
 #------ Cadastro Abastecimento -------#		
 	
 #-------- Excluir Abstecimento -------#	
 Exclui:
+	jal RData
+	bne $s7,$zero,ExcluiRealmente
+	li $v0,4
+	la $a0,SemReg
+	syscall
+	j Menu
+ExcluiRealmente:
+	
+	
 #-------- Excluir Abstecimento -------#	
 
 #--------- Exibe Abastecimento -------#	
-EAbastece:
+EAbastece: # FORMAT <DD>/<MM>/<AAAA> | <INT>Km | <INT> litros (<FLOAT> R$/l) | Posto <posto>
+	jal RData
+	addi $t0, $sp, -28
+TryNxt:	addi $t0, $t0, 28
+	lw  $t1, 0($t0)
+	sll $t1, $t1, 16
+	srl $t1, $t1, 16 # Crop EPOCH data
+	bne $v0, $t0, TryNxt 
+	
+	li  $v0, 4
+	la  $a0, Ex_Qlmt
+	syscall
+	li  $v0, 1
+	lw  $a0, 8($s0)
+	syscall
+	
+	j Menu
+No_Val:	
+	
+	j Menu
 #--------- Exibe Abastecimento -------#	
 
 #---------- Exibe Consumo ------------#	
@@ -84,7 +164,7 @@ EMedio:
 
 #------ Converte Data para EPOCH -----#
 DateToEpoch: #DD em $a0 - MM em $a1 - AAAA em $a2
-	addi $t1, $a1, -1 # Janeiro é mes 1
+	addi $t1, $a1, -1 # Janeiro � mes 1
 	mul  $t1, $t1, 30
 	
 	addi $t2, $a2, -2000 # EPOCH em 2000
@@ -107,10 +187,43 @@ EpochToDate: #EPOCH em $v0
 	sub  $a0, $t0, $t1 # Resto em $a0 (dia)
 
 	addi $a2, $a2, 2000 # EPOCH em 2000
-	addi $a1, $a1, 1 # Janeiro é mes 1
+	addi $a1, $a1, 1 # Janeiro � mes 1
 	
 	jr $ra
 #------ Converte EPOCH para Data -----#
+#------------ Recebe data ------------#
+RData:	
+	li $v0,4	#Recebe Dia em $a0
+	la $a0,Ins_Dia
+	syscall
+	li $v0,5
+	syscall
+	add $t7,$zero,$v0
 	
+	li $v0,4	#Recebe M�s em $a1
+	la $a0,Ins_Mes
+	syscall
+	li $v0,5
+	syscall
+	add $t6,$zero,$v0
+	
+	li $v0,4	#Recebe Ano em $a2
+	la $a0,Ins_Ano
+	syscall
+	li $v0,5
+	syscall
+	add $t5,$zero,$v0
+	
+	add $a0,$zero,$t7
+	add $a1,$zero,$t6
+	add $a2,$zero,$t5
+	
+	add $t4,$zero,$ra
+	jal DateToEpoch
+	
+	jr $t4
+#------------ Recebe data ------------#
 Exit:
-	
+	li $v0,17
+	li $a0,0
+	syscall
