982abd1fc75938cdea29a09086a57fd42ec4a8a1
marcosatsf <marcos_atsf@hotmail.com>
2018-08-23 11:21:24 -0300

    Update ProjetoArq.asm
    - Added strcmp

--- ProjetoArq.asm
+++ ProjetoArq.asm
@@ -1,7 +1,6 @@
 .data
 ReadString:	.space 	16	#Nome do Posto
-StructInfo:	.space	28	#Número dado em bytes: | Data-2 | QtdCombust�vel-2 | Pre�o-4 | Distancia-4 | NomePosto-16
-
+StructInfo:	.space	32	#Número dado em bytes: | Data-2 | QtdCombust�vel-2 | Pre�o-4 | Distancia-4 | NomePosto-16 | PonteiroProx-4
 
 #Declaração de strings
 Cadastrar:      .asciiz "\n1.Cadastrar abastecimento;\n" 
@@ -24,7 +23,12 @@ SemReg:		.asciiz "Não há registro de abastecimento, retornando ao menu...\n"
 Ex_Qlmt:	.asciiz "Quilometragem: "
 .text
 #----------- Inicializando -----------#
+	#subi $sp, $sp, -28 
+	divu $sp, $sp, 32
+	mulu $sp, $sp, 32
 	and $s7,$s7,$zero	#"Seta" $s7 para 0 pois este contar� quantos cadastros foram feitos	
+	add $fp,$sp,$zero	#Escreve o valor maximo da pilha em FP
+	add $s6,$zero,$zero	#O ponteiro inicial será guardado em $s6
 #------------ Exibir Menu ------------#
 Menu:
 	li  $v0, 4
@@ -88,7 +92,6 @@ Cadastro:
 	li $v0,5
 	syscall
 	sll $v0,$v0,16
-	add $t3,$zero,$t1
 	or $t1,$t1,$v0
 	
 	li $v0,4	#Recebe Pre�o do litro em $f0
@@ -99,42 +102,62 @@ Cadastro:
 	
 	la $s4,ReadString
 	
-	beq $s7,$zero,InserePilha
-	add $fp,$zero,$sp
-	lh $t2,0($fp)
-	ble $t3,$t2,InserePilha
-	addi $fp,$fp,28
-ProximaCelula:
-	lh $t2,0($fp)
-	addi $t4,$zero,7
-	ble $t3,$t2,DeslocaCelula
-	j InserePilha
+	#addi $sp,$sp,-28
+	jal malloc
+	add $t7,$v0,$zero #current pointer in t7
 	
-DeslocaCelula:
-	lw $t5,0($fp)
-	sw $t5,-28($fp)
-	addi $fp,$fp,4
-	addi $t4,$t4,-1
-	bne $t4,$zero,DeslocaCelula
-	j ProximaCelula
-	
-InserePilha:	
-	addi $sp,$sp,-28
-	
-	sw $t1,0($sp)	#Data e Qtd Comb. OK
-	s.s $f0,4($sp)	#Pre�o do litro	OK
-	sw $s1,8($sp)	#Km Atual	O
-	addi $sp,$sp,0xc
+	sw $t1,0($v0)	#Data e Qtd Comb. OK
+	s.s $f0,4($v0)	#Pre�o do litro	OK
+	sw $s1,8($v0)	#Km Atual	O
+	addi $v0,$v0,0xc
 	addi $t0,$zero,4
 StoreWord:		#Nome do Posto
 	lw $t1,0($s4)
-	sw $t1,0($sp)
+	sw $t1,0($v0)
 	addi $s4,$s4,4
-	addi $sp,$sp,4
+	addi $v0,$v0,4
 	addi $t0,$t0,-1
 	bnez $t0,StoreWord
 	
-	addi $sp,$sp,-28
+	add  $v0, $t7, $zero
+	beq  $s7, $zero, emptyList #if list is empty
+	
+	
+	
+	lw   $t4, 0($v0) #store current item epoch
+	andi $t4, $t4, 65535 #crop epoch data ???
+	lw   $t1, 0($s6) 
+	andi $t1, $t1, 65535 #crop epoch data ???
+	
+	slt  $t3, $t4, $t1 #t3 = 1 if first data < new data
+	bne  $t3, $zero, emptyList
+	
+	add  $t0, $s6, $zero	#Pega inicio da lista ligada
+findNext:
+	lw   $t2, 28($t0) #t2 is current->next
+	beq  $t2, $zero, exitFindNext
+	
+	lw   $t1, 0($t2) #t1 is current->next->data
+	andi $t1, $t1, 65535 #crop epoch data ???
+	
+	slt  $t3, $t1, $t4 #t3 is 1 if current->next->data < new data
+	beq  $t3, $zero, exitFindNext
+	
+	add  $t0, $t2, $zero #keep walking along list
+	j findNext
+exitFindNext:
+	sw   $t2, 28($v0) #new node -> next = current -> next
+	sw   $v0, 28($t0) #current -> next = current addr
+	j doneAdding
+	
+emptyList:
+	sw  $s6,28($v0) #new node -> next = old pointer
+	add $s6, $v0, $zero #old pointer = new node addr
+	#j doneAdding
+	
+	#sw $zero,0($v0) #Ponteiro "seta prox"
+	#addi $sp,$sp,-32
+doneAdding:
 	addi $s7,$s7,1
 		
 	j Menu	
@@ -142,38 +165,22 @@ StoreWord:		#Nome do Posto
 	
 #-------- Excluir Abstecimento -------#	
 Exclui:
-	jal RData
-	bne $s7,$zero,ExcluiRealmente
-	li $v0,4
-	la $a0,SemReg
-	syscall
+	add $t0,$fp,-64
+	sw $zero,0($t0)
 	j Menu
-ExcluiRealmente:
+	#jal RData
+	#bne $s7,$zero,ExcluiRealmente
+	#li $v0,4
+	#la $a0,SemReg
+	#syscall
+	#j Menu
+#ExcluiRealmente:
 	
 	
 #-------- Excluir Abstecimento -------#	
 
 #--------- Exibe Abastecimento -------#	
-EAbastece: # FORMAT <DD>/<MM>/<AAAA> | <INT>Km | <INT> litros (<FLOAT> R$/l) | Posto <posto>
-	jal RData
-	addi $t0, $sp, -28
-TryNxt:	addi $t0, $t0, 28
-	lw  $t1, 0($t0)
-	sll $t1, $t1, 16
-	srl $t1, $t1, 16 # Crop EPOCH data
-	bne $v0, $t0, TryNxt 
-	
-	li  $v0, 4
-	la  $a0, Ex_Qlmt
-	syscall
-	li  $v0, 1
-	lw  $a0, 8($s0)
-	syscall
-	
-	j Menu
-No_Val:	
-	
-	j Menu
+
 #--------- Exibe Abastecimento -------#	
 
 #---------- Exibe Consumo ------------#	
@@ -182,6 +189,8 @@ EConsumo:
 
 #-------- Exibe Preco Medio ----------#	
 EMedio:
+	add $t0, $s6,$zero
+	
 #-------- Exibe Preco Medio ----------#	
 
 #------ Converte Data para EPOCH -----#
@@ -245,6 +254,49 @@ RData:
 	
 	jr $t4
 #------------ Recebe data ------------#
+
+#--------------- malloc --------------#
+malloc:
+	add  $t3, $fp, $zero
+	add  $t0, $sp, $zero
+	#add  $t0, $sp, 32	#$t0 will move through stack
+
+	#slt  $t2, $t0, $t3	#	check if we dont try to find an invalid position
+	#beq  $t2, $zero, blowup	#	then try next block
+	
+next:	lw   $t2, 0($t0)
+	beq  $t2, $zero, found	#if current block is empty
+	addi $t0, $t0, 32	#	else will check next block
+	
+	slt  $t2, $t0, $t3	#	check if we dont try to find an invalid position
+	bne  $t2, $zero, next	#	then try next block
+	
+blowup:	addi $v0, $sp, -32	#		else just add a new position at the top of stack
+	addi $sp, $sp, -32
+	jr   $ra
+
+found:	add  $v0, $t0, $zero	#return current pointer
+	jr   $ra
+#--------------- malloc --------------#
+#--------------- cmpstr --------------#
+cmpstr:	#$a0 - End. String 1, $a1 - End. String 2
+	li $t1,0($a0)
+	li $t2,0($a1)
+	
+	bne $t1,$t2,cmpstrExitFalse
+	beq $t1,$zero,cmpstrExitTrue
+	
+	addi $a0,$a0,4
+	addi $a1,$a1,4
+	
+	j cmpstr
+cmpstrExitTrue:
+	addi $v0,$zero,1	#Retorna 1 caso a string seja igual
+	jr $ra
+cmpstrExitFalse:
+	addi $v0,$zero,$zero	#Retorna 0 caso a string seja diferente
+	jr $ra	
+#--------------- cmpstr --------------#
 Exit:
 	li $v0,17
 	li $a0,0
